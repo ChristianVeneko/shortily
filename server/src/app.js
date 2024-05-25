35d2414e989ac18aded19faf39a1dc5465e9dcda
Christian Mendoza <christianmendozater@gmail.com>
2024-05-24 21:47:48 -0400

    app config

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/api", authRoutes);
+
+export default app;
