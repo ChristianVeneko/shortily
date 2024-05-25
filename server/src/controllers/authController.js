a1d71e19d612e8b3cb83b9a87289dd7df7b022c5
Christian Mendoza <christianmendozater@gmail.com>
2024-05-24 21:50:33 -0400

    AuthController.js

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,56 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { supabase } from "../services/supabaseService.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+export const register = async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 8);
+
+  try {
+    const { data, error } = await supabase
+      .from("users")
+      .insert([{ username, password: hashedPassword }]);
+    if (error) throw error;
+    res
+      .status(200)
+      .json({ success: true, message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const { data: users, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("username", username);
+    if (error) throw error;
+
+    if (users.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const user = users[0];
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    if (!passwordIsValid) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: 86400 });
+    res.status(200).json({ success: true, message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
