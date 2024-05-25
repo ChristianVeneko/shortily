b72a4d090bda6c70214ffec85d5b045708c056e4
Christian Mendoza <christianmendozater@gmail.com>
2024-05-24 22:00:17 -0400

    Register.vue

---
+++ shortily/src/components/Register.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const username = ref('');
+const password = ref('');
+const router = useRouter();
+
+const register = async () => {
+    const response = await fetch('http://localhost:3000/api/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: username.value, password: password.value })
+    });
+
+    const data = await response.json();
+    if (data.success) {
+        alert(data.message);
+        router.push('/');
+    } else {
+        alert(data.message);
+    }
+};
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="mt-5">Register</h1>
+        <form @submit.prevent="register">
+            <div class="form-group">
+                <label for="regUsername">Username</label>
+                <input type="text" v-model="username" class="form-control" id="regUsername" required>
+            </div>
+            <div class="form-group">
+                <label for="regPassword">Password</label>
+                <input type="password" v-model="password" class="form-control" id="regPassword" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </form>
+        <p class="mt-3">Already have an account? <router-link to="/">Login here</router-link></p>
+    </div>
+</template>
