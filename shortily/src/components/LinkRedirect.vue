5ffd99129e1b6121e6942e79d04396d62ad5ad92
Christian Mendoza <christianmendozater@gmail.com>
2024-05-25 18:11:15 -0400

    api url config in redirect

--- shortily/src/components/LinkRedirect.vue
+++ shortily/src/components/LinkRedirect.vue
@@ -8,11 +8,12 @@ import { useRoute, useRouter } from 'vue-router';
 
 const route = useRoute();
 const router = useRouter();
+const API_URL = import.meta.env.VITE_API_URL
 
 onMounted(async () => {
     const shortUrl = route.params.shortUrl;
     try {
-        const response = await fetch(`http://localhost:3000/api/links/${shortUrl}`);
+        const response = await fetch(`${API_URL}/api/links/${shortUrl}`);
         const data = await response.json();
         if (data.success) {
             window.location.href = data.data.original_url;
